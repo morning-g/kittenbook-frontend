2adbd51bbaee7792761f282f5329afed7575cd7a
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-10-22 20:22:36 -0500

    Tareas

--- src/componentes/Tareas.js
+++ src/componentes/Tareas.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
-export default function Tareas(props) {
+export default function ColorTabs() {
+  const [value, setValue] = React.useState('one');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <h1>Tareas</h1>
-    </div>
+    <Box sx={{ width: '100%' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+        variant = "fullWidth"
+      >
+        <Tab value="one" label="Ecuaciones diferenciales" />
+        <Tab value="two" label="Item Two" />
+        <Tab value="three" label="Item Three" />
+        <Tab value="Cuarto" label="Item 4" />
+      </Tabs>
+    </Box>
   );
 }
