99dfcc3eafc29db6a612498e414033fd26e55bd3
MarioKaiju <marioavazquez2011@hotmail.com>
2021-10-28 20:35:54 -0500

    Tareas, funcionalidad agregar Tabs agregada. 28/10/2021

--- src/componentes/Tareas.js
+++ src/componentes/Tareas.js
@@ -85,16 +85,58 @@ import TextField from '@mui/material/TextField'
 import DialogTitle from '@mui/material/DialogTitle'
 import DialogContentText from '@mui/material/DialogContentText'
 import Add from '@mui/icons-material/Add'
-import Slide from '@mui/material/Slide'
+import Typography from '@mui/material/Typography'
+import PropTypes from 'prop-types'
 
+function TabPanel (props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+    role = "tabpanel"
+    hidden = {value !== index }
+    id = {`simple-tabpanel-${index}`}
+    aria-labelledby={`simple-tab-${index}`}
+    { ...other}
+    >
+      {value === index && (
+        <Box sx={{p:3}}>
+          <Typography>{children}</Typography>
+          <h1>Tabpanel</h1>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export default function Tareas() {
+  return (
+    <Box sx={{width:'100%'}}>
+      <ListaTabs/>      
+    </Box>
+  )
+}
+
+function ListaTabs() {
   // Arreglo de objetos tabs
-  const [tabs, setTabs] = useState ([{titulo:"Calculo", value:1},
-   {titulo:"Ecuaciones", value:2},
-   {titulo:"Gestión de Proyectos de Software", value:3},
-   {titulo:"Inteligencia Artificial", value:4},
-   {titulo:"Lenguajes y Automatas II", value:5}
+  const [tabs, setTabs] = useState ([{titulo:"Calculo", value:0},
+   {titulo:"Ecuaciones", value:1},
+   {titulo:"Gestión de Proyectos de Software", value:2},
+   {titulo:"Inteligencia Artificial", value:3},
+   {titulo:"Lenguajes y Automatas II", value:4}
   ]);
 
   // Ref al input del Dialog para insertar categoría
@@ -143,59 +185,61 @@ export default function Tareas() {
     setError(false);
     setHelperText("");
   };
-
-  return (
-    <Box sx={{ width: '95%', maxWidth: 1000, margin: "0 auto", justifyContent:"center", display:"flex"}}>
-      <ListaTabs tabs={tabs}/>
-        <Button onClick={handleClickOpen} variant="outlined" size="small" color="primary"
-         endIcon={<Add/>} sx={{minWidth:"110px"}}>
-           Agregar
-        </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Agregar categoría</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              inserta un título para la nueva categoría.
-              </DialogContentText>
-                <TextField
-                  inputRef={tabTituloRef}
-                  autoFocus
-                  margin="dense"
-                  id="titulo"
-                  label="Titulo"
-                  type="text"
-                  fullWidth
-                  helperText={helpertext}
-                  variant="standard"
-                  error = {error}
-                />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancelar</Button>
-            <Button onClick={handleAddTab}>Agregar</Button>
-          </DialogActions>
-        </Dialog>
-    </Box>
-  )
-}
-
-function ListaTabs({tabs}) {
+  
   const handleChange = (event, newValue) => {
       setValue(newValue);
   };
   const [value, setValue] = React.useState(1);
   return (
-      <Tabs
-      value={value}
-      onChange={handleChange}
-      textColor="secondary"
-      aria-label="scrollable auto tabs example"
-      variant = "scrollable">
-      {/* <Tab label="cinco" value="cinco"/> */}
-      {tabs.map((tab) => (
-          <Tab value={tab.value} label={tab.titulo}/>
-      ))}
-      </Tabs>
+    <div>
+      <Box sx={{ width: '95%', maxWidth: 1000, margin: "0 auto", justifyContent:"center", display:"flex"}}>
+          <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          aria-label="scrollable auto tabs example"
+          variant = "scrollable">
+          {tabs.map((tab) => (
+              <Tab value={tab.value} label={tab.titulo}/>
+          ))}
+          </Tabs>
+          <Button onClick={handleClickOpen} variant="outlined" size="small" color="primary"
+          endIcon={<Add/>} sx={{minWidth:"110px"}}>
+            Agregar
+          </Button>
+      </Box>
+      <div>
+        {tabs.map((tab) => (
+          <TabPanel>
+
+          </TabPanel>
+        ))}
+      </div>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Agregar categoría</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            inserta un título para la nueva categoría.
+            </DialogContentText>
+              <TextField
+                inputRef={tabTituloRef}
+                autoFocus
+                margin="dense"
+                id="titulo"
+                label="Titulo"
+                type="text"
+                fullWidth
+                helperText={helpertext}
+                variant="standard"
+                error = {error}
+              />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={handleAddTab}>Agregar</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   )
 }
 
