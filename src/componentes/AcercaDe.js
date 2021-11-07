81329a9efddc7bf4fd48c83fb8bac0eba1f0c5a3
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-11-06 22:24:29 -0600

    Acerca de terminado?

--- src/componentes/AcercaDe.js
+++ src/componentes/AcercaDe.js
@@ -1,28 +1,41 @@
 import React from "react";
-import styles from "./estilo.css";
 import Container from "@mui/material/Container";
-import logo from "../bookish.png";
+import logo from "../owlogo.png";
+import logoKitten from "../bookish.png";
 export default function AcercaDe(props) {
+  const EstiloLogo = {
+  };
+  const mision = { fontSize: "1.2rem" };
   return (
     <Container maxWidth="md">
       <div>
-        <h3>Kitten Book un producto de OmegaWare</h3>
-        <p>
-          Omega Ware es una empresa dedicada a la creación de programas
-          (software) que actualmente esta establecida en la ciudad de Torreón,
-          Coahuila
+        <h3>Acerca de OmegaWare</h3>
+       
+          <div style={EstiloLogo}>
+            <img src={logo} width="25%" />
+            <p style={mision}>
+              Omega Ware es una empresa dedicada a la creación de programas
+              (software) que actualmente esta establecida en la ciudad de Torreón,
+              Coahuila<br/><br/>
+              Nuestra mision es brindar una solución estable y eficiente a
+              nuestros clientes, que sea segura, cómoda y fácil de usar para
+              mejorar el rendimiento de su empresa y el de sus empleados.<br/><br/>
+              La empresa comienza lanzando su app kittenbook la cual se enfoca en
+              mejorar la organización de los estudiantes mediante la categorización
+              de sus recursos más comunes: horario, retícula, notas, tareas, y
+              créditos.<br/> 
+            </p>
           <br />
+          </div>
           <br />
-          La empresa comienza lanzando su app kittenbook la cual se enfoca en
-          mejorar la organización de los estudiantes mediante la categorización
-          de sus recursos más comunes: horario, retícula, notas, tareas, y
-          créditos.
-          <br />
-          La app surge de la necesidad de tener un software dedicado a la vida
-          estudiantil
-          <br />
-          <br />.
-        </p>
+          <div style={EstiloLogo}>
+            <img src={logoKitten} width="60%" />
+            <p style={mision}>
+              Nuestra primera app y con la que se pretende crear un mercado de
+              usuarios universitarios, cuenta con modalidades gratis y paga.<br/><br/>  
+            </p>
+            <h2 align="center">¡Tu vida estudiantil al alcance de tu mano!</h2>
+          </div>
       </div>
     </Container>
   );
